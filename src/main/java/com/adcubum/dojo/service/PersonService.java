160d5a578f97297816c629276d2647824ec4f677
Zeljko Ruzak <zeljko.ruzak@adcubum.com>
2019-11-20 15:26:02 +0100

    F Person service sets correct last name during creation

--- src/main/java/com/adcubum/dojo/service/PersonService.java
+++ src/main/java/com/adcubum/dojo/service/PersonService.java
@@ -6,6 +6,7 @@ public class PersonService {
     public Person create(String firstNameInput, String lastNameInput) {
         Person p = new Person();
         p.setFirstName(firstNameInput);
+        p.setLastName(lastNameInput);
         return p;
     }
 }
