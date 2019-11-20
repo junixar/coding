e5d75d8ff358523bc0ffc04e62a162a0312ed495
Christian Hofmann <christian.hofmann@adcubum.com>
2019-11-20 16:07:15 +0100

    r parameter validation extracted to separate method

--- src/main/java/com/adcubum/dojo/service/PersonService.java
+++ src/main/java/com/adcubum/dojo/service/PersonService.java
@@ -4,17 +4,18 @@ import com.adcubum.dojo.domain.Person;
 
 public class PersonService {
     public Person create(String firstNameInput, String lastNameInput) {
-
-        if (firstNameInput.trim().isEmpty()) {
-            throw  new IllegalArgumentException();
-        }
-
-        if (lastNameInput.trim().isEmpty()) {
-            throw  new IllegalArgumentException();
-        }
-
+        validateInputParameters(firstNameInput, lastNameInput);
         return new Person(firstNameInput, lastNameInput);
     }
 
+    private void validateInputParameters(String firstNameInput, String lastNameInput) {
+        if (firstNameInput.trim().isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+
+        if (lastNameInput.trim().isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+    }
 
 }
